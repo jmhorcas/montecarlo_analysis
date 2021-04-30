55a9b953c8536fe5d6faec00fdde5ea2dd874e15
José Miguel Horcas <jhorcas@us.es>
2021-04-30 12:54:18 +0200

    fix probabilities in simulations

--- main_bdd_analysis.py
+++ main_bdd_analysis.py
@@ -16,7 +16,7 @@ AAFMFRAMEWORK_FM = '(AAFMFramework) && (!python-sat || APISolvers) && (!pycosat 
 JHIPSTER = '(JHipster) && (!HazelCast || Hibernate2ndLvlCache) && (!EhCache || Hibernate2ndLvlCache) && (!Hibernate2ndLvlCache || EhCache || HazelCast) && (!HazelCast || !EhCache) && (!MicroserviceApplication || Server) && (!UaaServer || Server) && (!Server || MicroserviceApplication || UaaServer) && (!UaaServer || !MicroserviceApplication) && (!Gradle || BackEnd) && (!Maven || BackEnd) && (!BackEnd || Maven || Gradle) && (!Gradle || !Maven) && (!Generator || JHipster) && (!Authentication || JHipster) && (!SocialLogin || JHipster) && (!Database || JHipster) && (!SpringWebSockets || JHipster) && (!Libsass || JHipster) && (!ClusteredSession || JHipster) && (!BackEnd || JHipster) && (!InternationalizationSupport || JHipster) && (!Docker || JHipster) && (!TestingFrameworks || JHipster) && (!JHipster || Generator) && (!JHipster || Authentication) && (!JHipster || BackEnd) && (!JHipster || TestingFrameworks) && (!MySQL || Production) && (!MariaDB || Production) && (!PostgreSQL || Production) && (!Production || PostgreSQL || MySQL || MariaDB) && (!MariaDB || !MySQL) && (!MySQL || !PostgreSQL) && (!MariaDB || !PostgreSQL) && (!SQL || Database) && (!Cassandra || Database) && (!MongoDB || Database) && (!Database || Cassandra || SQL || MongoDB) && (!SQL || !Cassandra) && (!MongoDB || !SQL) && (!MongoDB || !Cassandra) && (!H2 || Development) && (!PostgreSQLDev || Development) && (!MariaDBDev || Development) && (!MySql || Development) && (!Development || MySql || H2 || PostgreSQLDev || MariaDBDev) && (!PostgreSQLDev || !H2) && (!MariaDBDev || !H2) && (!H2 || !MySql) && (!MariaDBDev || !PostgreSQLDev) && (!PostgreSQLDev || !MySql) && (!MariaDBDev || !MySql) && (!DiskBased || H2) && (!InMemory || H2) && (!H2 || InMemory || DiskBased) && (!DiskBased || !InMemory) && (!Hibernate2ndLvlCache || SQL) && (!Development || SQL) && (!Production || SQL) && (!ElasticSearch || SQL) && (!SQL || Development) && (!SQL || Production) && (!HTTPSession || Authentication) && (!OAuth2 || Authentication) && (!Uaa || Authentication) && (!JWT || Authentication) && (!Authentication || OAuth2 || JWT || HTTPSession || Uaa) && (!HTTPSession || !OAuth2) && (!Uaa || !HTTPSession) && (!HTTPSession || !JWT) && (!Uaa || !OAuth2) && (!JWT || !OAuth2) && (!Uaa || !JWT) && (!Protractor || TestingFrameworks) && (!Gatling || TestingFrameworks) && (!Cucumber || TestingFrameworks) && (!TestingFrameworks || Gatling) && (!TestingFrameworks || Cucumber) && (!MicroserviceGateway || Application) && (!Monolithic || Application) && (!Application || MicroserviceGateway || Monolithic) && (!Monolithic || !MicroserviceGateway) && (!Server || Generator) && (!Application || Generator) && (!Generator || Server || Application) && (!Application || !Server) && (!OAuth2 || SocialLogin || MicroserviceApplication || SQL || MongoDB) && (!SocialLogin || HTTPSession || JWT) && (!SocialLogin || SQL || MongoDB) && (!SocialLogin || Monolithic) && (!UaaServer || Uaa) && (OAuth2 || SocialLogin || MicroserviceApplication || SQL || MongoDB || Cassandra) && (!Server || !Protractor) && (Server || Protractor) && (!MySQL || H2 || MySql) && (JWT || Uaa || !MicroserviceApplication) && (JWT || Uaa || !MicroserviceGateway) && (!Monolithic || JWT || HTTPSession || OAuth2) && (!MariaDB || H2 || MariaDBDev) && (!PostgreSQL || H2 || PostgreSQLDev) && (Application || !SpringWebSockets) && (Application || !ClusteredSession) && (!Libsass || Application)'
 
 N_MC_SIMULATIONS = 100
-PERCENTAGE_SIMULATIONS = 0.05
+PERCENTAGE_SIMULATIONS = 0.01
 RUNS = 30
 
 def main():
@@ -63,7 +63,7 @@ def main():
                 combi_probability_defective_configs = round(len(combi_defective_configs) / len(combi_configurations), 2)
 
                 # Monte Carlo simulations
-                simulations = math.ceil(len(vp_configurations) * PERCENTAGE_SIMULATIONS)
+                simulations = math.ceil(len(combi_configurations) * PERCENTAGE_SIMULATIONS)
                 if simulations > len(combi_configurations):
                     simulations = len(combi_configurations)
                 runs = RUNS
@@ -76,7 +76,7 @@ def main():
                 median_defective_configs_in_sample = round(statistics.median(defective_configs_in_sample_per_runs), 2)
                 mean_defective_configs_in_sample = round(statistics.mean(defective_configs_in_sample_per_runs), 2)
                 std_defective_configs_in_sample = round(statistics.stdev(defective_configs_in_sample_per_runs), 2)
-                sample_median_probability_defective_configs = round(median_defective_configs_in_sample / len(combi_configurations), 2)
+                sample_median_probability_defective_configs = round(median_defective_configs_in_sample / simulations, 2)
 
                 file.write(f'{len(vps)}, {vp.name}, {len(vp_configurations)}, {len(vp_defective_configs)}, {vp_probability_defective_configs}, {len(combinations)}, ' \
                             + f'"{[str(f) for f in combi]}", {len(combi)}, {len(combi_configurations)}, {len(combi_defective_configs)}, {combi_probability_defective_configs}, ' \
